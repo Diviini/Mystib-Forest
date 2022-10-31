7e8f2f5fa548d80b73b40873cf6dc3da5b8d55a2
Diviini <robin.lebrozec@gmail.com>
2022-10-31 10:21:44 +0100

    Ajout du projet
    Ajout du projet

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public Rigidbody2D body;
+    public Vector3 velocity = Vector3.zero;
+
+    public bool isJumping;
+    public bool isGrounded;
+
+    public Animator animator;
+
+    public Transform LFootCheck;
+    public Transform RFootCheck;
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapArea(LFootCheck.position, RFootCheck.position);
+
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+                {
+                    isJumping = true;
+                }
+        
+        MovePlayer(horizontalMovement);
+
+        Flip(body.velocity.x);
+
+        float characterVelocity = Math.Abs(body.velocity.x);
+        animator.SetFloat("Speed", body.velocity.x);
+
+        
+    }
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, body.velocity.y);
+        body.velocity = Vector3.SmoothDamp(body.velocity, targetVelocity, ref velocity, .05f);
+
+        if (isJumping == true)
+        {
+            body.AddForce(new Vector2(0f, jumpForce));
+            isJumping=false;
+        }
+    }
+
+    void Flip( float _velocity)
+    {
+        if(_velocity < 0.1f)
+        {
+            SpriteRenderer.flipX
+        }
+    }
+}
